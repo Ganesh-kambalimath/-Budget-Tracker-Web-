4009fb04e98c5c52e3d28f18573f6c16a777327b
Ganesh.Kambalimath <156816974+Ganesh-kambalimath@users.noreply.github.com>
2025-08-04 02:18:30 +0530

    Update auth.config.js

--- server/config/auth.config.js
+++ server/config/auth.config.js
@@ -1,2 +1,7 @@
 const authConfig = {
-  secret: process.env.JWT_SECRET |
+  secret: process.env.JWT_SECRET || 'your_default_jwt_secret_if_not_in_env', //
+  jwtExpiration: '24h', 
+  jwtRefreshExpiration: '7d', 
+};
+
+export default authConfig;
