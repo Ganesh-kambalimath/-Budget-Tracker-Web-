b86c80aa4ab50159e999b3ce8e4fdc3d2ea2dc10
Ganesh.Kambalimath <156816974+Ganesh-kambalimath@users.noreply.github.com>
2025-08-04 00:50:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -45,3 +45,17 @@ app.get('/', (req, res) => {
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(err.statusCode |
+             | 500).json({
+message: err.message |
+
+| 'Something went wrong!',
+error: process.env.NODE_ENV === 'production'? {} : err.stack,
+});
+});
+
+const PORT = process.env.PORT |
+| 5000; 
+
+app.listen(PORT, () => {
+console.log(Server running on port ${PORT});
+});
